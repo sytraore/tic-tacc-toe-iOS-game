1d3db69aef0b6ff151e380fefe3ca65bb5e24650
Sy <stephanetraore46@gmail.com>
2023-11-07 01:33:46 -0500

    First try

--- ios101-capstone-project/SelectButton.swift
+++ ios101-capstone-project/SelectButton.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct SelectButton: View {
+    @Binding var letter: String
+    @State private var degrees = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Circle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.white)
+            Text(letter)
+                .font(.system(size:50))
+                .bold()
+        }
+        .rotation3DEffect(.degrees(degrees), axis:(x:0, y: 1, z:0))
+        .gesture(TapGesture()
+            .onEnded{ _ in
+                withAnimation(.easeIn(duration:0.25)){
+                    self.degrees -= 180
+                }
+                
+            })
     }
 }
 
 #Preview {
-    SelectButton()
+    SelectButton(letter: .constant("X"))
 }
