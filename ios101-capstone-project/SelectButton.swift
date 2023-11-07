d0d8fed1686cd5a2925a77b02d17aa110af2c414
Sy <stephanetraore46@gmail.com>
2023-11-07 01:32:19 -0500

    Begin project

---
+++ ios101-capstone-project/SelectButton.swift
@@ -0,0 +1,18 @@
+//
+//  SelectButton.swift
+//  ios101-capstone-project
+//
+//  Created by Stephane Traore on 11/7/23.
+//
+
+import SwiftUI
+
+struct SelectButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectButton()
+}
