0c17f6fcec499a197788d5469146911d87229b37
Sy <stephanetraore46@gmail.com>
2023-11-14 02:39:37 -0500

    late changes

--- ios101-capstone-project/ViewController.swift
+++ ios101-capstone-project/ViewController.swift
@@ -39,20 +39,117 @@ class ViewController: UIViewController {
     var currentTurn = Turn.Cross
     var O = "O"
     var X = "X"
+    var board = [UIButton]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // comment
         //view.backgroundColor = .red
-        
+        initBoard()
        
+    }
+    func initBoard(){
+        board.append(cell_1)
+        board.append(cell_2)
+        board.append(cell_3)
+        board.append(cell_4)
+        board.append(cell_5)
+        board.append(cell_6)
+        board.append(cell_7)
+        board.append(cell_8)
+        board.append(cell_9)
     }
     
     
     
     @IBAction func selectCell(_ sender: UIButton) {
         addToBoard(sender)
+        if winnerCheck(X){
+            result(title: "X win!")
+        }
+        if winnerCheck(O){
+            result(title: "O win!")
+        }
+        if (fullBoard()){
+            result(title: "Draw")
+        }
+    }
+    
+    func winnerCheck(_ s: String) -> Bool{
+        
+        // win horizontaly
+        if displayedSymbol(cell_1, s) && displayedSymbol(cell_2, s) && displayedSymbol(cell_3, s){
+            return true
+        }
+        if displayedSymbol(cell_4, s) && displayedSymbol(cell_5, s) && displayedSymbol(cell_6, s){
+            return true
+        }
+        if displayedSymbol(cell_7, s) && displayedSymbol(cell_8, s) && displayedSymbol(cell_9, s){
+            return true
+        }
+        
+        // win vertically
+        if displayedSymbol(cell_1, s) && displayedSymbol(cell_4, s) && displayedSymbol(cell_7, s){
+            return true
+        }
+        if displayedSymbol(cell_2, s) && displayedSymbol(cell_5, s) && displayedSymbol(cell_8, s){
+            return true
+        }
+        if displayedSymbol(cell_3, s) && displayedSymbol(cell_6, s) && displayedSymbol(cell_9, s){
+            return true
+        }
+        
+        // win diagonally
+        if displayedSymbol(cell_1, s) && displayedSymbol(cell_5, s) && displayedSymbol(cell_9, s){
+            return true
+        }
+        if displayedSymbol(cell_3, s) && displayedSymbol(cell_5, s) && displayedSymbol(cell_7, s){
+            return true
+        }
+        if displayedSymbol(cell_7, s) && displayedSymbol(cell_8, s) && displayedSymbol(cell_9, s){
+            return true
+        }
+        return false
+    }
+    
+    func displayedSymbol(_ button: UIButton, _ symbol: String) -> Bool{
+        return button.title(for: .normal) == symbol
+    }
+    
+    func result(title: String){
+        let c = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        c.addAction(UIAlertAction(title: "Restart", style: .default, handler: {(_) in
+            self.restartBoard()
+        }))
+        self.present(c, animated: true)
+    }
+    
+    func restartBoard(){
+        for button in board{
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if (firstTurn == Turn.NotCross){
+            firstTurn = Turn.Cross
+            turnlabel.text = X
+        }
+        else if (firstTurn == Turn.Cross){
+            firstTurn = Turn.NotCross
+            turnlabel.text = O
+        }
+        currentTurn = firstTurn
+
+    }
+    
+    func fullBoard() -> Bool{
+        for button in board{
+            if button.title(for: .normal) == nil{
+                return false
+            }
+        }
+        return true
     }
     
     func addToBoard(_ sender: UIButton){
@@ -67,6 +164,7 @@ class ViewController: UIViewController {
                 currentTurn = Turn.NotCross
                 turnlabel.text = O
             }
+            sender.isEnabled = false
         }
         
     }
