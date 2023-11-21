234e3a948d7b84ee1c1b6e9ded0b18a24776e7ff
Sy <stephanetraore46@gmail.com>
2023-11-20 23:46:07 -0500

    Change background color and cell letters colors for a nicer user interface

--- ios101-capstone-project/ViewController.swift
+++ ios101-capstone-project/ViewController.swift
@@ -46,7 +46,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // comment
-        //view.backgroundColor = .red
+        view.backgroundColor = .orange
         initBoard()
        
     }
